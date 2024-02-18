3e4f271e06da3b1ac6d1bd9a7beeaf55be600f0b
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-02-18 08:58:47 +0200

    added searchbar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,19 @@
+export const Searchbar = ({ onSubmit  }) => {
+  return (
+    <header className="searchbar">
+      <form className="form" onSubmit={onSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
+
+        <input
+          className="input"
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
