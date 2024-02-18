bb4b151f58ea34f314eed9bc102f982525f6831b
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-02-18 10:01:00 +0200

    added more styles

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import css from './ImageGalleryItem.module.scss';
+
 export const ImageGalleryItem = ({ image, onClick }) => {
   return (
-    <li className="gallery-item">
+    <li className={css.galleryItem}>
       <img
         src={image.webformatURL}
         alt={image.tags}
